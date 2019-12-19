3ea68cbfac2cab91dfde653abff1faca868f3669
c43721 <55610086+c43721@users.noreply.github.com>
2019-12-19 13:34:21 -0600

    Update prefixcache.ts

--- src/scheduled/prefixcache.ts
+++ src/scheduled/prefixcache.ts
@@ -1,6 +1,6 @@
 import { Client } from "../lib/types";
-import client from "../client";
 import { initPrefixCache } from "../util/prefix";
+import { close } from "../util/database";
 
 export const every = 1000 * 60 * 15; // 5 mins
 
@@ -8,5 +8,6 @@ export async function run(client: Client) {
     console.log("Caching prefixes...");
     client.cache.prefix = {};
     initPrefixCache();
+    await close();
     console.log("Updated prefixes.");
 }

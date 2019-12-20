f990037c88919ca56e903c6634439905f5e599d9
c43721 <55610086+c43721@users.noreply.github.com>
2019-12-20 15:50:40 -0600

    ETF2L Thread previews working.

--- src/preload/auto/etf2l.ts
+++ src/preload/auto/etf2l.ts
@@ -11,7 +11,7 @@ export const zones = ["text", "dm"];
 export async function run(client: Client, msg: Message) {
     const url = matchMsg(msg);
 
-    let screenshotBuffer = await captureSelector("https://" + url, "#content > div > div > table.pls");
+    let screenshotBuffer = await captureSelector("http://" + url, "#content > div > div > table.pls");
 
     msg.channel.send({
         files: [screenshotBuffer]

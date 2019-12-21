03020b0d8a1d7de5ca6fd8a9cc27dcdbb67cb1a8
c43721 <55610086+c43721@users.noreply.github.com>
2019-12-20 18:43:49 -0600

    Update prefix.ts

--- src/lib/external/prefix.ts
+++ src/lib/external/prefix.ts
@@ -1,10 +1,11 @@
 import { query } from "../../util/database";
 import client from "../../client";
+import config from "../../config";
 
 export default async function getGuildPrefix(id: string): Promise<string> {
     if (client.cache.prefix[id]) return client.cache.prefix[id]
     let rows = await query(`SELECT * FROM prefix WHERE guild='${id}'`);
-    if (!rows.length) return "!" as string;
+    if (!rows.length) return config.PREFIX as string;
     let prefix = rows[0].prefix.replace(".", " ");
     return prefix;
 }

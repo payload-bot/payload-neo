670f3cd3b3b3645ae2935a866fb16d5a75da6c4a
c43721 <55610086+c43721@users.noreply.github.com>
2020-04-09 23:44:11 -0500

    Prep for website

--- src/lib/model/Server.ts
+++ src/lib/model/Server.ts
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
+import { PermissionString } from "discord.js"
 
 export type ServerModel = mongoose.Document & {
     id?: string,
     prefix?: string,
     language?: string,
 
-    commandRestrictions?: Array<{channelID: string, commands: Array<string>}>,
+    commandRestrictions?: Array<{ channelID: string, commands: Array<string> }>,
+
     fun?: {
         payloadFeetPushed: number,
         payloadBeingDefended: boolean,
         payloadDefendTimeout: number
+    }
+
+    settings?: {
+        dashboardPermRoles: Array<PermissionString>,
+        pushcartLanguage: string
     }
 };
 
@@ -28,6 +35,11 @@ const serverSchema = new mongoose.Schema({
         payloadBeingDefended: Boolean,
         payloadDefendTimeout: Number
     },
+
+    settings: {
+        dashboardPermRoles: [String],
+        pushcartLanguage: String
+    }
 });
 
 export const Server = mongoose.model("Server", serverSchema);

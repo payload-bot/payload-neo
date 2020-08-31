dca2d7e3be62242f5a616b7708ab5aa69e424594
c43721 <55610086+c43721@users.noreply.github.com>
2020-08-31 14:41:17 -0500

    Space

--- src/events/log.ts
+++ src/events/log.ts
@@ -5,7 +5,7 @@ import { Client } from "../lib/types";
 module.exports = {
     run: async (client: Client, item: string) => {
         const channel = client.channels.cache.get(config.info.errorChannel) as TextChannel;
-        if(!channel) return;
+        if (!channel) return;
         channel.send({
             embed: {
                 color: 3066993,

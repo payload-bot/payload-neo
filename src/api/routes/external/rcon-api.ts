3e439b44a651faa648cc91c73b0c8ff3e7e2b5b9
c43721 <rlahman.game@gmail.com>
2020-12-27 18:09:03 -0600

    Change rcon-api to send 200

--- src/api/routes/external/rcon-api.ts
+++ src/api/routes/external/rcon-api.ts
@@ -12,8 +12,7 @@ router.post("/", async (req: Request, res: Response) => {
 		const { data } = await axios.post("https://rcon.tf/api/execute", req.body);
 		res.json({ success: true, body: data });
 	} catch (err) {
-		console.log(err.toString());
-		res.status(err.statusCode).json({ success: false, error: err.toString() });
+		res.status(200).json({ success: false, error: err.toString() });
 	}
 });
 

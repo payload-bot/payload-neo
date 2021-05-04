9393bbf0a5287b71ee0907d4d9c8917555ac7263
c43721 <rlahman.game@gmail.com>
2021-05-04 17:45:51 -0500

    hotfix: Temporarily bypass new apis' cache

--- src/api/routes/external/rgl-api.ts
+++ src/api/routes/external/rgl-api.ts
@@ -9,13 +9,21 @@ const router = express.Router();
 router.get("/:id", async (req: Request, res: Response) => {
 	const idParam = req.params.id;
 
+	if (!idParam) {
+		return res.status(400).json({
+			statusCode: 400,
+			error: "Bad request",
+			message: "Please enter an ID to search"
+		})
+	}
+
 	// this big thing does most of the hard work for us...
 	// we just have to validate errors and destructure for the tf2-helper chrome extension
 	// so that they don't have to update their repo (yet)
 	try {
 		const {
 			data: { data }
-		} = await axios.get<RglApiResponse>(`https://rgl.payload.tf/api/v1/profiles/${idParam}`);
+		} = await axios.get<RglApiResponse>(`https://rgl.payload.tf/api/v1/profiles/${idParam}/cache-bypass`);
 
 		return res.json(data);
 	} catch (err) {

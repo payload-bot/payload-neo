83441fdb49fef7fe1242fca8ae0b33381ff5aa83
c43721 <rlahman.game@gmail.com>
2021-06-26 18:19:05 -0500

    fix: Super duper fix staging redirect

--- src/api/middleware/setClientUrl.ts
+++ src/api/middleware/setClientUrl.ts
@@ -5,6 +5,6 @@ const STAGING_URL = "https://staging.payload.tf/";
 export const cookieName = "redirect-url";
 
 export default function setClientUrl(req: Request, res: Response, next: NextFunction) {
-    if (req.headers.referer === STAGING_URL) res.cookie(cookieName, STAGING_URL.slice(-1));
+    if (req.headers.referer === STAGING_URL) res.cookie(cookieName, STAGING_URL.slice(0, -1));
     next();
 }

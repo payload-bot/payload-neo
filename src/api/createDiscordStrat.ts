8b8ba7e963f6d605264e877cf79358f2c4fafc5d
c43721 <rlahman.game@gmail.com>
2021-06-26 16:47:52 -0500

    fix: Fix formatting in createDiscordStrat file

--- src/api/createDiscordStrat.ts
+++ src/api/createDiscordStrat.ts
@@ -4,39 +4,39 @@ import { User, UserModel } from "../lib/model/User";
 import { DiscordProfile, DiscordUserDetails } from "./interfaces";
 
 export default function createDiscordStrategy() {
-	return new DiscordStrategy(
-		{
-			clientID: process.env.CLIENT_ID,
-			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: process.env.CALLBACK_URL,
-			scope: ["identify", "guilds", "connections"]
-		},
-		async (accessToken, refreshToken, profile, cb) => {
-			let user: UserModel;
+    return new DiscordStrategy(
+        {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: process.env.CALLBACK_URL,
+            scope: ["identify", "guilds", "connections"],
+        },
+        async (accessToken, refreshToken, profile, cb) => {
+            let user: UserModel;
 
-			user = await User.findOne({ id: profile.id });
+            user = await User.findOne({ id: profile.id });
 
-			if (!user) {
-				user = await User.create({
-					id: profile.id
-				});
-			}
+            if (!user) {
+                user = await User.create({
+                    id: profile.id,
+                });
+            }
 
-			const { guilds, ...userProfile }: DiscordProfile = {
-				avatarUrl: `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}`,
-				fullName: `${profile.username}#${profile.discriminator}`,
-				isAdmin: config.allowedID === profile.id,
-				...profile
-			};
+            const { guilds, ...userProfile }: DiscordProfile = {
+                avatarUrl: `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}`,
+                fullName: `${profile.username}#${profile.discriminator}`,
+                isAdmin: config.allowedID === profile.id,
+                ...profile,
+            };
 
-			const returnObject: DiscordUserDetails = {
-				profile: userProfile,
-				accessToken,
-				refreshToken,
-				user
-			};
+            const returnObject: DiscordUserDetails = {
+                profile: userProfile,
+                accessToken,
+                refreshToken,
+                user,
+            };
 
-			cb(null, returnObject as any);
-		}
-	);
+            cb(null, returnObject as any);
+        }
+    );
 }

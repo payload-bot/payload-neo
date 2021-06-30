9615aacba9de170b925e2fa8dc4f223b1def9840
c43721 <rlahman.game@gmail.com>
2021-06-29 20:10:19 -0500

    feat(migration): Format migrations, add steamId migration

---
+++ migrations/1625015164470-rename-steamid-field.js
@@ -0,0 +1,30 @@
+"use strict";
+const { MongoClient } = require("mongodb");
+const { config } = require("dotenv");
+
+/*
+  Renames steamID to steamId in User model
+*/
+
+module.exports.up = function (next) {
+    config();
+
+    MongoClient.connect(process.env.MONGO_URI, { useUnifiedTopology: true })
+        .then(c => c.db())
+        .then(db => db.collection("users"))
+        .then(collection =>
+            collection.updateMany(
+                {
+                    steamID: {
+                        $exists: 1,
+                    },
+                },
+                {
+                    $rename: {
+                        'steamID': 'steamId',
+                    },
+                }
+            )
+        )
+        .then(() => next());
+};

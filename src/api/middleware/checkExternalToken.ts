f9b336b63864b7f0b918b16a3d9b0ccc39865d4b
c43721 <rlahman.game@gmail.com>
2021-07-03 20:42:07 -0500

    fix: Fix status codes in external token middleware

--- src/api/middleware/checkExternalToken.ts
+++ src/api/middleware/checkExternalToken.ts
@@ -5,14 +5,16 @@ export default function checkExternalToken(req: Request, res: Response, next: Ne
     const client = req.headers["x-payload-client"];
 
     switch (client) {
-		// Incoming hooks for when there's a TF2 Update
+        // Incoming hooks for when there's a TF2 Update
         case "TF2-UPDATE": {
             const isTokenVerified = process.env.TF2SCRAPER_WEBHOOK_TOKEN === incomingToken;
-			console.log(isTokenVerified)
-            return isTokenVerified ? next() : res.status(403).send({ message: "Forbidden" });
+            console.log(isTokenVerified);
+            return isTokenVerified
+                ? next()
+                : res.status(403).json({ status: 403, message: "Forbidden" });
         }
         default: {
-            return res.status(403).send({ message: "Forbidden" });
+            return res.status(404).json({ status: 404, message: "Not found" });
         }
     }
 }

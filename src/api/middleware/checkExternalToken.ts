6784f8efaa60411235e67000e0182f2e51730a57
c43721 <rlahman.game@gmail.com>
2021-07-05 14:58:32 -0500

    fix: Remove testing log

--- src/api/middleware/checkExternalToken.ts
+++ src/api/middleware/checkExternalToken.ts
@@ -8,7 +8,6 @@ export default function checkExternalToken(req: Request, res: Response, next: Ne
         // Incoming hooks for when there's a TF2 Update
         case "TF2-UPDATE": {
             const isTokenVerified = process.env.TF2SCRAPER_WEBHOOK_TOKEN === incomingToken;
-            console.log(isTokenVerified);
             return isTokenVerified
                 ? next()
                 : res.status(403).json({ status: 403, message: "Forbidden" });

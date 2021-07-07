b9c788677893cb2b9267fc7b9ef191d4d49a5062
c43721 <rlahman.game@gmail.com>
2021-07-06 20:58:46 -0500

    feat: Allow empty steamId

--- src/api/validators/user-settings.ts
+++ src/api/validators/user-settings.ts
@@ -2,7 +2,7 @@ import {number, object, string } from "joi";
 import { NotificationLevel } from "../../util/push-notification";
 
 const userSettingsSchema = object({
-    steamId: string().pattern(/(765611\d{11})/),
+    steamId: string().allow('').regex(/(765611\d{11})/).optional().default(null),
     notificationsLevel: number().min(NotificationLevel.NONE).max(NotificationLevel.ALL),
 });
 

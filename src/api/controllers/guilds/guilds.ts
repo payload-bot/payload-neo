c39506118ef20d386ca5d581c1db6d47c9470831
c43721 <55610086+c43721@users.noreply.github.com>
2021-07-31 10:53:37 -0500

    Release API for public (#88)
    * refactor: Make util function for fetching client guild
    
    * fix: Await the call
    
    * refactor: Remove beta check to guild routes
    
    * refactor: Refactor guild routes
    
    * refactor: Remove deprecated stat route
    
    * refactor: Remove json space from server config
    
    * refactor: Reformat index.ts
    
    * fix: Fix import
    
    * chore(deps): Update dependencies

--- src/api/controllers/guilds/guilds.ts
+++ src/api/controllers/guilds/guilds.ts
@@ -1,12 +1,12 @@
 import { Router, Request, Response } from "express";
 import client from "../../..";
 import checkAuth from "../../middleware/checkAuth";
-import checkBeta from "../../middleware/checkBeta";
 import checkServers from "../../middleware/checkServers";
 import DiscordService from "../../services/DiscordService";
 import GuildService from "../../services/GuildService";
 import UserService from "../../services/UserService";
 import WebhookService from "../../services/WebhookService";
+import getDiscordGuild from "../../utils/getDiscordGuild";
 import guildSettingsSchema from "../../validators/guild-settings";
 
 const router = Router();
@@ -17,76 +17,86 @@ const userService = new UserService();
 const webhookService = new WebhookService();
 
 router.use(checkAuth);
-router.use(checkBeta);
 
 router.get("/", async (req: Request, res: Response) => {
-    const user = req.user;
+  const user = req.user;
 
-    const { accessToken, refreshToken } = await userService.getUserByDiscordId(user.id);
+  const { accessToken, refreshToken } = await userService.getUserByDiscordId(
+    user.id
+  );
 
-    const guilds = await discordService.getAuthedGuilds(user.id, accessToken, refreshToken);
+  const guilds = await discordService.getAuthedGuilds(
+    user.id,
+    accessToken,
+    refreshToken
+  );
 
-    res.json(guilds.map(({ server, ...guild }) => guild));
+  res.json(guilds.map(({ server, ...guild }) => guild));
 });
 
 router.get("/:guildId", checkServers, async (req: Request, res: Response) => {
-    const {
-        enableSnipeForEveryone = false,
-        language = "en-US",
-        prefix = "pls ",
-        webhook,
-        fun,
-        commandRestrictions,
-        id,
-    } = req.guild;
+  const {
+    enableSnipeForEveryone = false,
+    language = "en-US",
+    prefix = "pls ",
+    webhook,
+    fun,
+    commandRestrictions,
+    id,
+  } = req.guild;
 
-    const { icon, name, channels } = client.guilds.cache.get(id) ?? (await client.guilds.fetch(id));
-    const bot = client.guilds.cache.get(id).members.cache.get(client.user.id);
+  const { icon, name, channels } = await getDiscordGuild(id);
+  const bot = client.guilds.cache.get(id).members.cache.get(client.user.id);
 
-    res.json({
-        guild: {
-            channels: channels.cache
-                .filter(c => c.type === "text")
-                .map(({ id, name }) => ({ id, name })),
-        },
-        commands: {
-            restrictions: commandRestrictions,
-            commands: client.commands.filter(c => !c.requiresRoot).map(c => c.name),
-            autoResponses: client.autoResponses.map(c => c.name),
-        },
-        icon: icon && `https://cdn.discordapp.com/icons/${id}/${icon}.png`,
-        botName: bot.nickname ?? bot.user.username,
-        webhook: (await webhookService.getWebhookById(webhook)) || null,
-        enableSnipeForEveryone,
-        name,
-        id,
-        fun,
-        language,
-        prefix,
-    });
+  res.json({
+    guild: {
+      channels: channels.cache
+        .filter((c) => c.type === "text")
+        .map(({ id, name }) => ({ id, name })),
+    },
+    commands: {
+      restrictions: commandRestrictions,
+      commands: client.commands
+        .filter((c) => !c.requiresRoot)
+        .map((c) => c.name),
+      autoResponses: client.autoResponses.map((c) => c.name),
+    },
+    icon: icon && `https://cdn.discordapp.com/icons/${id}/${icon}.png`,
+    botName: bot.nickname ?? bot.user.username,
+    webhook: (await webhookService.getWebhookById(webhook)) || null,
+    enableSnipeForEveryone,
+    name,
+    id,
+    fun,
+    language,
+    prefix,
+  });
 });
 
 router.patch("/:guildId", checkServers, async (req: Request, res: Response) => {
-    try {
-        const { botName, ...values } = await guildSettingsSchema.validateAsync(req.body);
+  try {
+    const { botName, ...values } = await guildSettingsSchema.validateAsync(
+      req.body
+    );
 
-        if (botName) {
-            const bot = client.guilds.cache
-                .get(req.params.guildId)
-                .members.cache.get(client.user.id);
-            bot.setNickname(botName);
-        }
-
-        if (values) {
-            await guildService.findByGuildIdAndUpdate(req.params.guildId, values);
-        }
-
-        return res.status(204).send();
-    } catch (err) {
-        return res
-            .status(400)
-            .json({ status: 400, error: "Bad request", message: err.details.map(d => d.message) });
+    if (botName) {
+      const guild = await getDiscordGuild(req.params.guildId);
+      const bot = guild.members.cache.get(client.user.id);
+      bot.setNickname(botName);
     }
+
+    if (values) {
+      await guildService.findByGuildIdAndUpdate(req.params.guildId, values);
+    }
+
+    return res.status(204).send();
+  } catch (err) {
+    return res.status(400).json({
+      status: 400,
+      error: "Bad request",
+      message: err.details.map((d) => d.message),
+    });
+  }
 });
 
 export default router;
--- src/api/index.ts
+++ src/api/index.ts
@@ -14,62 +14,47 @@ import GuildRoutes from "./controllers/guilds";
 import WebhookRoutes from "./controllers/webhooks";
 
 export async function listen(port: number): Promise<void> {
-    const server = express();
+  const server = express();
 
-    server.use(express.json());
-    server.use(express.urlencoded({ extended: false }));
-    server.set("json spaces", 1);
-    server.use(
-        cors({
-            origin: process.env.CLIENT_URL,
-        })
-    );
-    server.use(helmet());
-    server.use(cookie());
-    server.use(passport.initialize());
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: false }));
+  server.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+    })
+  );
+  server.use(helmet());
+  server.use(cookie());
+  server.use(passport.initialize());
 
-    passport.serializeUser((user, done) => {
-        done(null, user);
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.deserializeUser((obj, done) => {
+    done(null, obj as any);
+  });
+
+  const discordStrategy = createDiscordStrategy();
+  passport.use(discordStrategy);
+  refresh.use("discord", discordStrategy);
+
+  server.use("/api/stats", cors(), StatRoutes);
+  server.use("/api/auth/discord", DiscordAuthRoutes);
+  server.use("/api/auth", AuthRoutes);
+  server.use("/api/users", UserRoutes);
+  server.use("/api/guilds", GuildRoutes);
+  server.use("/api/webhooks/v1", WebhookRoutes);
+
+  server.all("*", (req: Request, res: Response) => {
+    res.status(404).json({
+      status: 404,
+      error: "Not found",
+      message: `Cannot find ${req.method} route for ${req.path}`,
     });
+  });
 
-    passport.deserializeUser((obj, done) => {
-        done(null, obj as any);
-    });
-
-    const discordStrategy = createDiscordStrategy();
-    passport.use(discordStrategy);
-    refresh.use("discord", discordStrategy);
-
-    // For some reason, vercel
-    // Won't deploy without it. Why it didn't do it a week ago?
-    // Who knows!
-    server.use(
-        "/api/internal/public/",
-        cors(),
-        (req, res, next) => {
-            // Deprecate this route and notify people
-            res.setHeader("Warning", '299 - "Deprecated"');
-            next();
-        },
-        StatRoutes
-    );
-    
-    server.use("/api/stats", cors(), StatRoutes);
-    server.use("/api/auth/discord", DiscordAuthRoutes);
-    server.use("/api/auth", AuthRoutes);
-    server.use("/api/users", UserRoutes);
-    server.use("/api/guilds", GuildRoutes);
-    server.use("/api/webhooks/v1", WebhookRoutes);
-
-    server.all("*", (req: Request, res: Response) => {
-        res.status(404).json({
-            status: 404,
-            error: "Not found",
-            message: `Cannot find ${req.method} route for ${req.path}`,
-        });
-    });
-
-    return new Promise(resolve => {
-        server.listen(port, resolve);
-    });
+  return new Promise((resolve) => {
+    server.listen(port, resolve);
+  });
 }

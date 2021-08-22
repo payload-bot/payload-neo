81f328bc10b0052148f9a0d8517901440f83bbed
c43721 <55610086+c43721@users.noreply.github.com>
2021-08-21 23:33:46 -0500

    fix: fix more error redirects (#92)

--- src/api/middleware/verifyLoginState.ts
+++ src/api/middleware/verifyLoginState.ts
@@ -2,26 +2,27 @@ import { Request, Response, NextFunction } from "express";
 
 export const stateCookieName = "discord-state";
 
-export default async function setDiscordState(req: Request, res: Response, next: NextFunction) {
-    const stateFromCookie = req.cookies?.[stateCookieName];
-    const stateFromDiscord = req.query.state;
+export default async function setDiscordState(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const stateFromCookie = req.cookies?.[stateCookieName];
+  const stateFromDiscord = req.query.state;
 
-    if (!stateFromCookie) {
-        // Clear cookie no matter what
-        res.clearCookie(stateCookieName);
+  res.clearCookie(stateCookieName);
 
-        return res.redirect(`${process.env.CLIENT_URL}/login/failure?message=Bad request`);
-    }
+  if (!stateFromCookie) {
+    return res.redirect(
+      `${process.env.CLIENT_URL}/login/error?message=Bad request`
+    );
+  }
 
-    if (stateFromDiscord !== stateFromCookie) {
-        // Clear cookie no matter what
-        res.clearCookie(stateCookieName);
-        return res.redirect(
-            `${process.env.CLIENT_URL}/login/failure?message=Could not complete request`
-        );
-    }
+  if (stateFromDiscord !== stateFromCookie) {
+    return res.redirect(
+      `${process.env.CLIENT_URL}/login/error?message=Could not complete request`
+    );
+  }
 
-    res.clearCookie(stateCookieName);
-
-    next();
+  next();
 }

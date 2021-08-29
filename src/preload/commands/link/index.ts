7d0bf646759f61777bf2b41a596841a6045232a1
c43721 <rlahman.game@gmail.com>
2021-08-29 11:35:29 -0500

    refactor!: more refactoring

--- src/preload/commands/link/index.ts
+++ src/preload/commands/link/index.ts
@@ -1,47 +1,53 @@
 import { Command } from "../../../lib/exec/Command";
 import { Client } from "../../../lib/types";
-import { Message } from "discord.js";
+import { Message, MessageButton } from "discord.js";
 import { ensureSteamID } from "../../../util/steam-id";
 import Language from "../../../lib/types/Language";
 
 export default class Link extends Command {
-    constructor() {
-        super(
-            "link",
-            "Links your steam account to your Discord account.",
-            [
-                {
-                    name: "Steam ID",
-                    description: "Your Steam ID. Can be in any of the popular Steam ID formats.",
-                    required: true,
-                    type: "string"
-                }
-            ]
-        );
+  constructor() {
+    super("link", "Links your steam account to your Discord account.", [
+      {
+        name: "Steam ID",
+        description:
+          "Your Steam ID. Can be in any of the popular Steam ID formats.",
+        required: true,
+        type: "string",
+      },
+    ]);
+  }
+
+  async run(client: Client, msg: Message): Promise<boolean> {
+    const args = await this.parseArgs(msg);
+    const lang: Language = await this.getLanguage(msg);
+
+    if (args === false) {
+      return false;
     }
 
-    async run(client: Client, msg: Message): Promise<boolean> {
-        const args = await this.parseArgs(msg);
-        const lang: Language = await this.getLanguage(msg);
+    const steamIDTestResult = await ensureSteamID(args[0] as string);
 
-        if (args === false) {
-            return false;
-        }
+    if (!steamIDTestResult || !steamIDTestResult.length) {
+      await msg.reply({
+        content: lang.link_fail_invalidid,
+      });
 
-        const steamIDTestResult = await ensureSteamID(args[0] as string);
-
-        if (!steamIDTestResult || !steamIDTestResult.length) {
-            return await this.fail(msg, lang.link_fail_invalidid);
-        }
-
-        const user = await client.userManager.getUser(msg.author.id);
-
-        user.user.steamId = steamIDTestResult;
-
-        await user.save();
-
-        await this.respond(msg, lang.link_success.replace('%steamid', steamIDTestResult).replace('%tag', msg.author.tag));
-
-        return true;
+      return false;
     }
+
+    const user = await client.userManager.getUser(msg.author.id);
+
+    user.user.steamId = steamIDTestResult;
+
+    await user.save();
+
+    await this.respond(
+      msg,
+      lang.link_success
+        .replace("%steamid", steamIDTestResult)
+        .replace("%tag", msg.author.tag)
+    );
+
+    return true;
+  }
 }
--- src/preload/commands/settings/index.ts
+++ src/preload/commands/settings/index.ts
@@ -2,53 +2,56 @@ import { Command } from "../../../lib/exec/Command";
 import { Client } from "../../../lib/types/Client";
 import { Message } from "discord.js";
 
-import PermissionCommand from "./permissions"
+import PermissionCommand from "./permissions";
 import Language from "../../../lib/types/Language";
 
-
 export default class Settings extends Command {
+  constructor() {
+    super(
+      "settings",
+      "Guild settings",
+      [
+        {
+          name: "subcommand",
+          description: "Name of subcommand to execute",
+          required: true,
+          type: "string",
+        },
+        {
+          name: "subcommand arg",
+          description: "Arguments for subcommand",
+          required: false,
+          type: "string",
+        },
+      ],
+      undefined,
+      ["ADMINISTRATOR"],
+      ["GUILD_TEXT"],
+      undefined,
+      {
+        permissions: new PermissionCommand(),
+      }
+    );
+  }
 
-    constructor() {
-        super(
-            "settings",
-            "Guild settings",
-            [
-                {
-                    name: "subcommand",
-                    description: "Name of subcommand to execute",
-                    required: true,
-                    type: "string"
-                }, 
-                {
-                    name: "subcommand arg",
-                    description: "Arguments for subcommand",
-                    required: false,
-                    type: "string"
-                }
-            ],
-            undefined,
-            ["ADMINISTRATOR"],
-            ["GUILD_TEXT"],
-            undefined,
-            {
-                permissions: new PermissionCommand(),
-            }
+  async run(client: Client, msg: Message): Promise<boolean> {
+    const args = await this.getArgs(msg);
+    const lang: Language = await this.getLanguage(msg);
+
+    if (args[0]) {
+      if (!this.subCommands[args[0]]) {
+        await this.respond(
+          msg,
+          lang.settings_fail_nosubcmd.replace(
+            "%prefix",
+            await this.getPrefix(msg)
+          )
         );
+
+        return false;
+      }
+
+      return await this.runSub(args[0], client, msg);
     }
-
-    async run(client: Client, msg: Message): Promise<boolean> {
-        const args = await this.getArgs(msg)
-        const lang: Language = await this.getLanguage(msg)
-
-        if (args[0]) {
-            if (!this.subCommands[args[0]]) {
-                await this.respond(msg, lang.settings_fail_nosubcmd.replace('%prefix', await this.getPrefix(msg)));
-
-                return false;
-            }
-
-            return await this.runSub(args[0], client, msg);
-        }
-    }
-
+  }
 }

b212adda5d8b574547fb1db7a808b034340c2e27
c43721 <rlahman.game@gmail.com>
2021-08-29 11:49:19 -0500

    feat: allow redirects from preview sites

--- src/api/middleware/setClientUrl.ts
+++ src/api/middleware/setClientUrl.ts
@@ -1,10 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 
-const STAGING_URL = "https://staging.payload.tf/";
+const ALLOWED_HOSTS = ["https://payload.tf/", "https://vercel.app"];
 
 export const cookieName = "redirect-url";
 
-export default function setClientUrl(req: Request, res: Response, next: NextFunction) {
-    if (req.headers.referer === STAGING_URL) res.cookie(cookieName, STAGING_URL.slice(0, -1));
-    next();
+export default function setClientUrl(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (ALLOWED_HOSTS.includes(req.headers.referer)) {
+    res.cookie(cookieName, ALLOWED_HOSTS.indexOf(req.headers.host));
+  }
+
+  next();
 }

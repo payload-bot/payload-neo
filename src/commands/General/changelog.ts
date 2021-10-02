509fe5f095e0ef91a13c9fbedadca04ef032e3ee
satvik-1203 <satvikgsu@gmail.com>
2021-10-01 22:24:38 -0400

    fchanged return type of changelog

--- src/commands/General/changelog.ts
+++ src/commands/General/changelog.ts
@@ -2,7 +2,6 @@ import { Args, Command, CommandOptions } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
 import type { Message } from "discord.js";
 import { send } from "@sapphire/plugin-editable-commands";
-import getArgs from "#root/util/get-args";
 import { getChangelog } from "#root/util/get-changelog";
 import { codeBlock } from "@discordjs/builders";
 
@@ -12,8 +11,11 @@ import { codeBlock } from "@discordjs/builders";
 })
 export class UserCommand extends Command {
   async run(msg: Message, args: Args) {
-    const [version] = getArgs(args);
+    const argStr = await args.pick("string").catch(() => null);
 
+    if (!argStr) return await send(msg, { content: "Invalid version format" });
+
+    const [version] = argStr.split(" ");
     const changeLog = await getChangelog(version);
 
     if (!changeLog)
--- src/util/get-args.ts
+++
@@ -1,6 +0,0 @@
-import type { Args } from "@sapphire/framework";
-
-export default function getArgs(args: Args) {
-  const argArr = args.message.cleanContent.split(" ").slice(2);
-  return argArr;
-}

e2bc6369a4ae87f64e3d05326c88a85f899835d3
c43721 <rlahman.game@gmail.com>
2021-10-09 19:02:36 -0500

    feat: default connection string

--- src/lib/utils/connectMongo.ts
+++ src/lib/utils/connectMongo.ts
@@ -1,9 +1,14 @@
 import type { SapphireClient } from "@sapphire/framework";
 import { connect } from "mongoose";
 
+// Default to localhost
+process.env.MONGO_URI ??= "mongodb://localhost:27017/payload";
+
+console.log(process.env.MONGO_URI);
+
 export default async function connectMongo(client: SapphireClient) {
   try {
-    await connect(process.env.MONGO_URI as string);
+    await connect(process.env.MONGO_URI!);
     client.logger.info("Successfully connected to MongoDB.");
   } catch (err) {
     client.logger.error("Failed to connect to MongoDB. Check your password!");

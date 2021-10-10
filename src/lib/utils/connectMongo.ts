39c79e7e246c2afabbb3061ece771a02c5abb10f
c43721 <rlahman.game@gmail.com>
2021-10-09 19:37:27 -0500

    feat: fatal log when mongoose fails to connect

--- src/lib/utils/connectMongo.ts
+++ src/lib/utils/connectMongo.ts
@@ -1,17 +1,16 @@
 import type { SapphireClient } from "@sapphire/framework";
 import { connect } from "mongoose";
 
-// Default to localhost
-process.env.MONGO_URI ??= "mongodb://localhost:27017/payload";
-
-console.log(process.env.MONGO_URI);
-
 export default async function connectMongo(client: SapphireClient) {
   try {
-    await connect(process.env.MONGO_URI!);
+    await connect(process.env.MONGO_URI!, {
+      connectTimeoutMS: 5000,
+      dbName: "payload",
+    });
     client.logger.info("Successfully connected to MongoDB.");
   } catch (err) {
-    client.logger.error("Failed to connect to MongoDB. Check your password!");
+    client.logger.fatal("Failed to connect to MongoDB. Check your password!");
+    client.logger.fatal(err);
     client.destroy();
     process.exit(1);
   }

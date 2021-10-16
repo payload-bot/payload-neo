60155292592846873c728ffba27efc57f6e0bb61
c43721 <rlahman.game@gmail.com>
2021-10-15 19:33:15 -0500

    feat: add custom command struct

---
+++ src/lib/structs/commands/PayloadCommand.ts
@@ -0,0 +1,49 @@
+import { CommandContext, PieceContext, UserError } from '@sapphire/framework';
+import { fetchT } from '@sapphire/plugin-i18next';
+import { SubCommandPluginCommand } from '@sapphire/plugin-subcommands';
+import type { Message } from 'discord.js';
+import * as Lexure from 'lexure';
+import { PayloadArgs } from './PayloadArgs';
+
+export abstract class PayloadCommand extends SubCommandPluginCommand<PayloadCommand.Args, PayloadCommand> {
+	public readonly hidden: boolean;
+
+	public constructor(context: PieceContext, options: PayloadCommand.Options) {
+		super(context, { ...options });
+
+		this.hidden = options.hidden ?? false;
+	}
+
+	/**
+	 * The pre-parse method. This method can be overridden by plugins to define their own argument parser.
+	 * @param message The message that triggered the command.
+	 * @param parameters The raw parameters as a single string.
+	 * @param context The command-context used in this execution.
+	 */
+	public async preParse(message: Message, parameters: string, context: CommandContext): Promise<PayloadCommand.Args> {
+		const parser = new Lexure.Parser(this.lexer.setInput(parameters).lex()).setUnorderedStrategy(this.strategy);
+		const args = new Lexure.Args(parser.parse());
+        
+		return new PayloadArgs(message, this, args, context, await fetchT(message));
+	}
+
+	protected error(identifier: string | UserError, context?: unknown): never {
+		throw typeof identifier === 'string' ? new UserError({ identifier, context }) : identifier;
+	}
+
+	protected parseConstructorPreConditions(options: PayloadCommand.Options): void {
+		super.parseConstructorPreConditions(options);
+	}
+}
+
+export namespace PayloadCommand {
+	/**
+	 * The PayloadCommand Options
+	 */
+	export type Options = SubCommandPluginCommand.Options & {
+		hidden?: boolean;
+	};
+
+	export type Args = PayloadArgs;
+	export type Context = CommandContext;
+}

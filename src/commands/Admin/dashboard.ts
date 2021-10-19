a80b868283f2cb554c97e71e4af5835f8652909d
c43721 <rlahman.game@gmail.com>
2021-10-18 19:19:33 -0500

    fix(build): remove unused imports

--- src/commands/Admin/dashboard.ts
+++ src/commands/Admin/dashboard.ts
@@ -2,14 +2,13 @@ import { ApplyOptions } from "@sapphire/decorators";
 import { Message, MessageActionRow, MessageButton } from "discord.js";
 import { send } from "@sapphire/plugin-editable-commands";
 import { PayloadCommand } from "#lib/structs/commands/PayloadCommand";
-import { LanguageKeys } from "#lib/i18n";
 
 @ApplyOptions<PayloadCommand.Options>({
   description: "Links to the dashboard",
   runIn: ["GUILD_TEXT"],
 })
 export class UserCommand extends PayloadCommand {
-  async run(msg: Message, args: PayloadCommand.Args) {
+  async run(msg: Message) {
     const component = new MessageActionRow().addComponents([
       new MessageButton({
         url: `https://payload.tf/dashboard/${msg.guild!.id}`,

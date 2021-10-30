337324112f3a87394859b09c5e34b0c2703c7aa2
c43721 <rlahman.game@gmail.com>
2021-10-30 12:45:26 -0500

    feat: add descriptions for all commands

--- src/lib/i18n/all/keys/commands/info.ts
+++ src/lib/i18n/all/keys/commands/info.ts
@@ -2,5 +2,5 @@ import { T, FT } from "#lib/types";
 
 export const Description = T<string>("commands/info:description");
 export const DetailedDescription = T<string>("commands/info:detailedDescription");
-export const EmbedTitle = FT<{ users: number, servers: number }, string>("commands/info:title");
-export const EmbedDescription = T<string>("commands/info:description");
+export const EmbedTitle = FT<{ users: number, servers: number }, string>("commands/info:embedTitle");
+export const EmbedDescription = T<string>("commands/info:embedDescription");

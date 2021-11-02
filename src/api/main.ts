a0ef118dd5173fd836bad5894f7601ab42da6443
c43721 <rlahman.game@gmail.com>
2021-11-02 17:33:27 -0500

    feat: install packages and add cors

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,7 +1,13 @@
 import { NestFactory } from "@nestjs/core";
+import type { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.modules";
 
 export async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+  });
+
   await app.listen(3000);
 }

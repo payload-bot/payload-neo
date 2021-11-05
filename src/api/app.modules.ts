15c6899a51909094951d0deb53b657e14ca9194a
c43721 <rlahman.game@gmail.com>
2021-11-04 19:04:56 -0500

    feat: add mongodb module

--- src/api/app.modules.ts
+++ src/api/app.modules.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 import { PassportModule } from "@nestjs/passport";
 import { AuthModule } from "./auth/auth.module";
+import { Environment } from "./environment/environment";
+import { EnvironmentModule } from "./environment/environment.module";
 
 @Module({
-  imports: [AuthModule, PassportModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [EnvironmentModule],
+      inject: [Environment],
+      useFactory: async (environment: Environment) => ({
+        uri: environment.mongoDbUri,
+      }),
+    }),
+    PassportModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })

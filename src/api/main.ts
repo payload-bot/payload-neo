f467429d9a3c4b92587c82ff8b9dba093f5d04f5
c43721 <rlahman.game@gmail.com>
2021-11-08 20:49:03 -0600

    fix: correct cors

--- src/api/main.ts
+++ src/api/main.ts
@@ -2,6 +2,7 @@ import { VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import type { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.modules";
+import { Environment } from "./environment/environment";
 
 export async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,8 +15,10 @@ export async function bootstrap() {
     defaultVersion: "1",
   });
 
+  const env = app.get(Environment);
+
   app.enableCors({
-    origin: process.env.CLIENT_URL,
+    origin: env.clientUrl,
   });
 
   await app.listen(8080);

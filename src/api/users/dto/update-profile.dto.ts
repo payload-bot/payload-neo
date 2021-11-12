69c4e8e1b42fed6975099d89f9f62a90dd43eb56
c43721 <rlahman.game@gmail.com>
2021-11-11 18:48:27 -0600

    fix: fix order of decorators

--- src/api/users/dto/update-profile.dto.ts
+++ src/api/users/dto/update-profile.dto.ts
@@ -1,16 +1,16 @@
 import { IsValidSteamId } from "#api/shared/steamid.validator";
 import NotificationLevel from "#utils/notificationLevel";
-import { Optional } from "@nestjs/common";
-import { IsNumber, IsString, Max, Min } from "class-validator";
+import { IsNumber, IsOptional, IsString, Max, Min } from "class-validator";
 
 export class UpdateProfileDto {
+  @IsOptional()
   @IsValidSteamId()
-  @Optional()
   @IsString()
   steamId!: string;
 
+  @IsOptional()
+  @IsNumber()
   @Min(NotificationLevel.NONE)
   @Max(NotificationLevel.ALL)
-  @IsNumber()
   notificationsLevel!: number;
 }

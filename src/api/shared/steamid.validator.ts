65f7f570d8e5f23f240ca7700a7b21d46c9ef70d
c43721 <rlahman.game@gmail.com>
2021-11-11 18:43:14 -0600

    feat: custom validator for steamid

---
+++ src/api/shared/steamid.validator.ts
@@ -0,0 +1,24 @@
+import { registerDecorator, ValidationOptions } from "class-validator";
+import { ID as SteamId } from "@node-steam/id";
+
+export function IsValidSteamId(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: "isValidSteamId",
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      options: {
+        ...validationOptions,
+        message: "$value is not a valid steamid",
+      },
+      validator: {
+        validate(value: any) {
+          const steamId = new SteamId(value);
+
+          return typeof value === "string" && steamId.isValid();
+        },
+      },
+    });
+  };
+}

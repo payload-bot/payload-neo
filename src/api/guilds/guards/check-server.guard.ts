032a2526796b287d1da622acc48f57d62a56c69c
c43721 <rlahman.game@gmail.com>
2021-11-12 20:08:23 -0600

    feat: neat guard for enforcing readability of guilds

---
+++ src/api/guilds/guards/check-server.guard.ts
@@ -0,0 +1,36 @@
+import type { User } from "#api/users/models/user.model";
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  InternalServerErrorException,
+} from "@nestjs/common";
+import { container } from "@sapphire/framework";
+import { GuildsService } from "../services/guilds.service";
+
+const { client } = container;
+
+@Injectable()
+export class CheckServerGuard implements CanActivate {
+  constructor(private guildsService: GuildsService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const guildId = request.params.guildId;
+    const user = request.user as User;
+
+    const userServers = await this.guildsService.getUserGuilds(user.id);
+
+    if (!userServers.map((s) => s.id).includes(guildId)) {
+      return false;
+    }
+
+    if (!(await client.guilds.fetch(guildId))) {
+      throw new InternalServerErrorException({
+        message: "Could not fetch guild from Discord",
+      });
+    }
+
+    return true;
+  }
+}

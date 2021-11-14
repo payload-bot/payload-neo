3109be832e6d00c06efc88b5eca8502ae6d4bec2
c43721 <rlahman.game@gmail.com>
2021-11-13 20:30:38 -0600

    feat: add crud for webhooks

--- src/api/webhooks/services/webhook.service.ts
+++ src/api/webhooks/services/webhook.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
+import { plainToClass } from "class-transformer";
+import { generate } from "generate-password";
 import type { Model } from "mongoose";
-import { Webhook, WebhookDocument } from "../models/webhook.model";
+import {
+  Webhook,
+  WebhookDocument,
+  WebhookTargetType,
+} from "../models/webhook.model";
 
 @Injectable()
 export class WebhookService {
@@ -10,5 +16,31 @@ export class WebhookService {
     private webhookModel: Model<WebhookDocument>
   ) {}
 
-  
+  async getWebhookByDiscordId(id: string) {
+    const webhook = await this.webhookModel
+      .findOne({ id })
+      .orFail()
+      .lean()
+      .exec();
+
+    return plainToClass(Webhook, webhook);
+  }
+
+  async deleteWebhookByDiscordId(id: string) {
+    await this.webhookModel.findOneAndRemove({ id }).orFail().lean().exec();
+  }
+
+  async createNewWebhook(type: WebhookTargetType, id: string) {
+    const createdWebhook = await this.webhookModel.create({
+      id,
+      type,
+      value: generate({
+        length: 40,
+        numbers: true,
+        strict: true,
+      }),
+    });
+
+    return plainToClass(Webhook, createdWebhook);
+  }
 }

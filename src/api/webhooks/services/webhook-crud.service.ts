d8c3b5f23ff3113e7b5671da63e6278ef9a01f34
c43721 <rlahman.game@gmail.com>
2021-11-20 13:59:57 -0600

    chore(deps): update all dependencies

--- src/api/webhooks/services/webhook-crud.service.ts
+++ src/api/webhooks/services/webhook-crud.service.ts
@@ -1,5 +1,9 @@
 import { GuildsService } from "#api/guilds/services/guilds.service";
-import { BadRequestException, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { plainToClass } from "class-transformer";
 import { generate } from "generate-password";
@@ -37,10 +41,15 @@ export class WebhookCrudService {
   async getWebhookBySecret(secret: string) {
     const webhook = await this.webhookModel
       .findOne({ value: secret })
-      .orFail()
       .lean()
       .exec();
 
+    if (!webhook) {
+      throw new UnauthorizedException(
+        "The provided webhook secret is not valid"
+      );
+    }
+    
     return plainToClass(Webhook, webhook);
   }
 

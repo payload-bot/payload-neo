683a3082a5b61c2ebfdf966e9136955fcff08dc6
c43721 <rlahman.game@gmail.com>
2021-12-18 15:43:24 -0600

    chore: remove all jwt stuff

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,23 +1,14 @@
 import { DiscordModule } from "#api/discord/discord.module";
 import { UsersModule } from "#api/users/users.module";
 import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
 import { AuthController } from "./controllers/auth.controller";
-import { RefreshToken, RefreshTokenSchema } from "./models/refreshToken.model";
 import { AuthService } from "./services/auth.service";
 import { DiscordStrategy } from "./strategies/discord.strategy";
-import { JwtStrategy } from "./strategies/jwt.strategy";
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: RefreshToken.name, schema: RefreshTokenSchema },
-    ]),
-    UsersModule,
-    DiscordModule,
-  ],
+  imports: [UsersModule, DiscordModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, DiscordStrategy],
+  providers: [AuthService, DiscordStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
--- src/api/auth/controllers/auth.controller.ts
+++ src/api/auth/controllers/auth.controller.ts
@@ -1,6 +1,4 @@
 import {
-  BadRequestException,
-  Body,
   Controller,
   Get,
   HttpCode,
@@ -14,7 +12,6 @@ import type { Response } from "express";
 import { AuthGuard } from "@nestjs/passport";
 import { Environment } from "#api/environment/environment";
 import { AuthService } from "../services/auth.service";
-import { AuthContext } from "../interfaces/auth.interface";
 import { CurrentUser } from "../decorators/current-user.decorator";
 import { Auth } from "../guards/auth.guard";
 import type { User } from "#api/users/models/user.model";
@@ -38,42 +35,10 @@ export class AuthController {
   async callback(
     @CurrentUser() { id }: User,
     @Res({ passthrough: true }) res: Response
-  ) {
-    const authToken = await this.authService.generateJwtToken(
-      AuthContext.AUTH,
-      id
-    );
-
-    const refreshToken = await this.authService.generateJwtToken(
-      AuthContext.REFRESH,
-      id
-    );
-
-    const redirectUrl = this.environment.clientUrl;
-    res.redirect(
-      `${redirectUrl}/login/success?token=${authToken}&refreshToken=${refreshToken}`
-    );
-  }
-
-  @Post("refresh")
-  @HttpCode(HttpStatus.OK)
-  async refresh(@Body("refreshToken") token: string) {
-    if (!token) throw new BadRequestException();
-
-    return await this.authService.refreshTokens(token);
-  }
+  ) {}
 
   @Post("/logout")
   @HttpCode(HttpStatus.NO_CONTENT)
   @Auth()
-  async logout(
-    @Body("refreshToken") refreshToken: string,
-    @CurrentUser() { id, accessToken }: User
-  ) {
-    if (!refreshToken) throw new BadRequestException();
-    if (!accessToken) throw new BadRequestException();
-
-    await this.authService.logOut(id, refreshToken, accessToken);
-    return;
-  }
+  async logout(@CurrentUser() { id, accessToken }: User) {}
 }

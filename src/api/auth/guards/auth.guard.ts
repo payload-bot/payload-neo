2369daabd3ec99b6dfff77d8f516c373a0afcd9c
c43721 <rlahman.game@gmail.com>
2021-12-18 15:43:24 -0600

    chore: remove all jwt stuff

--- src/api/auth/guards/auth.guard.ts
+++ src/api/auth/guards/auth.guard.ts
@@ -2,5 +2,5 @@ import { applyDecorators, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 export function Auth() {
-  return applyDecorators(UseGuards(AuthGuard("jwt")));
+  return applyDecorators(UseGuards(AuthGuard("session")));
 }

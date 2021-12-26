21a5e80c2a8471080ea37aeb8691146bf17dadb0
c43721 <rlahman.game@gmail.com>
2021-12-26 11:53:21 -0600

    feat: start differentiating specific posts

--- src/auto/tftv.ts
+++ src/auto/tftv.ts
@@ -5,26 +5,39 @@ import {
 import { ApplyOptions } from "@sapphire/decorators";
 import PayloadColors from "#utils/colors";
 import { Message, MessageEmbed } from "discord.js";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import cheerio from "cheerio";
-import { convert  } from "html-to-text";
+import { convert } from "html-to-text";
 import { LanguageKeys } from "#lib/i18n/all";
+import { send } from "@sapphire/plugin-editable-commands";
 
 @ApplyOptions<AutoCommandOptions>({
   description: LanguageKeys.Auto.Tftv.Description,
-  regex: /teamfortress\.tv\/\d+\/[\w-]+/,
+  regex: /teamfortress\.tv\/\d+\/[\w-]+(#\d+)*/,
 })
 export default class UserAutoCommand extends AutoCommand {
   async messageRun(msg: Message) {
-    const url = "https://" + this.getMatch(msg);
-    const { data } = await axios(url, { responseType: "text" });
-    const $ = cheerio.load(data as string);
+    const match = this.getMatch(msg);
+
+    const url = "https://" + match;
+    const { data } = (await axios(url, {
+      responseType: "text",
+    })) as AxiosResponse<string>;
+    const $ = cheerio.load(data);
+
+    const title = $(".thread-header-title").text().trim();
+
+    const needFindChild = match.split("#")[1].length > 0;
+
+    if (needFindChild) {
+      // is a specific post
+    }
+
+    const $post = $(`#thread-container > .post:nth-child(1)`);
 
     const frags = $("#thread-frag-count").text().trim();
-    const title = $(".thread-header-title").text().trim();
-    const $post = $(`#thread-container > .post:nth-child(1)`);
     const author = $post.find(".post-header .post-author").text().trim();
-    const body = convert ($post.find(".post-body") as any as string);
+    const body = convert($post.find(".post-body-hidden") as unknown as string);
 
     const dateSelector = $post
       .find(".post-footer .js-date-toggle")
@@ -35,6 +48,10 @@ export default class UserAutoCommand extends AutoCommand {
       : "N/A";
 
     const embed = new MessageEmbed({
+      title,
+      url,
+      color: PayloadColors.USER,
+      timestamp: new Date(date),
       description: `${author}\n\n${
         body.length > 700
           ? `${body.slice(0, 700)}...\n[read more](${url})`
@@ -43,12 +60,8 @@ export default class UserAutoCommand extends AutoCommand {
       footer: {
         text: `${frags} frags`,
       },
-      color: PayloadColors.USER,
-      timestamp: new Date(date),
-      title,
-      url,
     });
 
-    return await msg.channel.send({ embeds: [embed] });
+    return await send(msg, { embeds: [embed] });
   }
 }

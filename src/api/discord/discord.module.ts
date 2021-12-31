24178ce7630f2b6fdf1a61e609cba9e2530690ac
c43721 <rlahman.game@gmail.com>
2021-12-31 12:32:05 -0600

    feat(cache): quick and dirty memory cache for guilds

--- src/api/discord/discord.module.ts
+++ src/api/discord/discord.module.ts
@@ -1,9 +1,9 @@
 import { HttpModule } from "@nestjs/axios";
-import { Module } from "@nestjs/common";
+import { CacheModule, Module } from "@nestjs/common";
 import { DiscordService } from "./services/discord.service";
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CacheModule.register()],
   providers: [DiscordService],
   exports: [DiscordService],
 })

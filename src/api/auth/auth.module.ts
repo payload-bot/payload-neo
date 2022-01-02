643c4d5ed099166c257017db5c66938aeac67468
c43721 <rlahman.game@gmail.com>
2022-01-01 21:06:03 -0600

    feat: correctly import passport module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,13 +1,14 @@
 import { DiscordModule } from "#api/discord/discord.module";
 import { UsersModule } from "#api/users/users.module";
 import { Module } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
 import { AuthController } from "./controllers/auth.controller";
 import { AuthService } from "./services/auth.service";
 import { SessionSerializer } from "./services/session.serializer";
 import { DiscordStrategy } from "./strategies/discord.strategy";
 
 @Module({
-  imports: [UsersModule, DiscordModule],
+  imports: [UsersModule, DiscordModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService, DiscordStrategy, SessionSerializer],
   exports: [AuthService],

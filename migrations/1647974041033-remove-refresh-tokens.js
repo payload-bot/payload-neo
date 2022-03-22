b98b2b6049faf7123e7512dfe8ca119538e24838
c43721 <rlahman.game@gmail.com>
2022-03-22 13:44:47 -0500

    feat(migration): remove refresh token migration

---
+++ migrations/1647974041033-remove-refresh-tokens.js
@@ -0,0 +1,17 @@
+"use strict";
+const { MongoClient } = require("mongodb");
+const { config } = require("dotenv");
+
+/*
+  Removes "RefreshTokens" database
+*/
+module.exports.up = function (next) {
+  config();
+
+  MongoClient.connect(process.env.MONGO_URI, { useUnifiedTopology: true })
+    .then((c) => c.db())
+    .then((db) => db.collection("refreshtokens"))
+    // No op on deletion - if it's deleted it's deleted. Don't care.
+    .then((collection) => collection.drop().catch(() => {}))
+    .then(() => next());
+};

bf3c76dc00dd7504d01866ca856000f2eba88644
c43721 <55610086+c43721@users.noreply.github.com>
2022-04-05 20:32:21 -0500

    refactor(cache): abstract cache   (#144)

--- src/api/app.modules.ts
+++ src/api/app.modules.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { MongooseModule } from "@nestjs/mongoose";
 import { AuthModule } from "./auth/auth.module";
 import { Environment } from "./environment/environment";
 import { EnvironmentModule } from "./environment/environment.module";
@@ -8,10 +7,13 @@ import { GuildsModule } from "./guilds/guilds.module";
 import { UsersModule } from "./users/users.module";
 import { WebhooksModule } from "./webhooks/webhooks.module";
 import { MigrationsModule } from "./migrations/migrations.module";
+import { CacheModule } from "./cache/cache.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    CacheModule.register(),
     MongooseModule.forRootAsync({
       imports: [EnvironmentModule],
       inject: [Environment],

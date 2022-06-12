b66e7ecea331a9f087e2ce28f084192011d58e26
c43721 <rlahman.game@gmail.com>
2022-06-11 20:01:51 -0500

    fix: bugger

--- src/api/users/user.repository.ts
+++ src/api/users/user.repository.ts
@@ -2,8 +2,6 @@ import { EntityRepository } from "#api/repository/EntityRepository";
 import type { User } from "#lib/models/User";
 
 export class UserRepository extends EntityRepository<typeof User> {
-  public model = "User";
-
   public override async postGet(obj: any) {
     this.logger.info(this.identity);
     this.logger.info(obj);

94bb66afac136f8bddd81cda2b04e406cef80fa5
c43721 <rlahman.game@gmail.com>
2022-06-26 13:23:07 -0500

    feat: yet another quality commit

--- src/routes/webhook/webhook-execution-test.route copy.ts
+++ src/routes/webhook/webhook-execution-test.route copy.ts
@@ -1,4 +1,6 @@
 import { ServiceController } from "#lib/api/ServiceController";
+import { sendTest } from "#lib/api/utils/webhook-helper";
+import { Webhook } from "#lib/models";
 import { ApplyOptions } from "@sapphire/decorators";
 import {
   type ApiRequest,
@@ -6,12 +8,27 @@ import {
   methods,
   type RouteOptions,
 } from "@sapphire/plugin-api";
+import { isNullish } from "@sapphire/utilities";
 
 @ApplyOptions<RouteOptions>({
   route: "webhooks/test",
 })
 export class WebhookTestRoute extends ServiceController {
   public async [methods.POST](request: ApiRequest, response: ApiResponse) {
-    // test
+    const headerAuth = request.headers?.authorization;
+
+    if (isNullish(headerAuth)) {
+      return response.unauthorized();
+    }
+
+    const webhook = await Webhook.findOne({ value: headerAuth }).lean().exec();
+
+    if (webhook == null) {
+      return response.notFound();
+    }
+
+    await sendTest(this.client, webhook.type, webhook.id);
+
+    return response.noContent();
   }
 }

be336a4593b75d6b854842301234280c294a3ac6
c43721 <rlahman.game@gmail.com>
2022-06-29 20:49:08 -0500

    fix: guild can be null

--- src/lib/api/utils/helpers.ts
+++ src/lib/api/utils/helpers.ts
@@ -6,13 +6,14 @@ export async function canManage(userId: string | undefined, guildId: string) {
     return false;
   }
 
-  const guild = await container.client.guilds.fetch(guildId);
+  const guild = await container.client.guilds.fetch(guildId).catch(() => null);
 
   if (guild == null) {
     return false;
   }
 
   const member = guild.members.cache.get(userId);
+  
   if (member == null) {
     return false;
   }

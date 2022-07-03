e7ffa9cf679e5cdeeb0d57e53817265411e5d0ea
c43721 <rlahman.game@gmail.com>
2022-07-03 15:06:47 -0500

    feat: add logging middleware

--- src/lib/utils/connectDatabase.ts
+++ src/lib/utils/connectDatabase.ts
@@ -1,10 +1,25 @@
 import { container } from "@sapphire/pieces";
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 export default async function connectPrisma() {
   const prisma = new PrismaClient();
+
+  prisma.$use(logger);
 
   await prisma.$connect();
 
   container.database = prisma;
 }
+
+const logger: Prisma.Middleware<any> = async (params, next) => {
+  const before = Date.now();
+
+  const result = await next(params);
+
+  const after = Date.now();
+
+  container.logger.debug(`Query ${params.model}.${params.action} took ${after - before}ms`);
+  container.logger.debug(JSON.stringify(params.args));
+
+  return result;
+};

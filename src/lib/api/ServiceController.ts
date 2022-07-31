dd43c6cc41fa576f8b20d1437faaffc7923e93f1
c43721 <rlahman.game@gmail.com>
2022-07-30 21:01:29 -0500

    refactor: cant use this anymore

--- src/lib/api/ServiceController.ts
+++ src/lib/api/ServiceController.ts
@@ -1,8 +1,7 @@
 import type { PrismaClient } from "@prisma/client";
 import type { PieceContext } from "@sapphire/framework";
-import { type ApiResponse, Route, type RouteOptions, type ApiRequest } from "@sapphire/plugin-api";
+import { type ApiResponse, Route, type RouteOptions } from "@sapphire/plugin-api";
 import { isNullish } from "@sapphire/utilities";
-import { EntityRepository } from "./repository/EntityRepository";
 
 export type Entity = PrismaClient["user"] | PrismaClient["guild"] | PrismaClient["webhook"];
 
@@ -23,9 +22,10 @@ export abstract class ServiceController extends Route {
     }
   }
 
-  protected createRepository(request: ApiRequest, response: ApiResponse, model: Entity) {
-    const identity = request.auth;
+  // BLOCKED UNTIL PRISMA FIGURES SOMETHING COOL OUT
+  // protected createRepository(request: ApiRequest, response: ApiResponse, model: Entity): IEntityRepository<Entity> {
+  //   const identity = request.auth;
 
-    return new EntityRepository<Entity>(model, request, response, identity!);
-  }
+  //   return new EntityRepository<Entity>(model, request, response, identity!);
+  // }
 }

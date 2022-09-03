34c431ea8f830bc1c804ac79f5559237209803c2
c43721 <rlahman.game@gmail.com>
2022-09-02 19:42:36 -0500

    chore(*): esm

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,10 @@ import { PayloadClient } from "#lib/PayloadClient";
 
 const client = new PayloadClient();
 
-async function main() {
-  try {
-    await client.login();
-  } catch (error) {
-    container.logger.error(error);
-    client.destroy();
-    process.exit(1);
-  }
+try {
+  await client.login();
+} catch (error) {
+  container.logger.error(error);
+  client.destroy();
+  process.exit(1);
 }
-
-main().catch(container.logger.error.bind(container.logger));

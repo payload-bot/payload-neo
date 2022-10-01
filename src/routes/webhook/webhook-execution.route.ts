51638fbeb4519443078dff2ff0c38eb465241b88
c43721 <55610086+c43721@users.noreply.github.com>
2022-10-01 10:53:46 -0500

    fix(webhook): add missing endpoints for backwards compatability (#162)

--- src/routes/webhook/webhook-execution.route.ts
+++ src/routes/webhook/webhook-execution.route.ts
@@ -10,6 +10,7 @@ const schema = s.object({
 }).strict;
 
 @ApplyOptions<RouteOptions>({
+  name: "webhooklogs",
   route: "webhooks/logs",
 })
 export class WebhookExecutionRoute extends ServiceController {
@@ -41,3 +42,40 @@ export class WebhookExecutionRoute extends ServiceController {
     return response.noContent();
   }
 }
+
+@ApplyOptions<RouteOptions>({
+  name: "webhooklogs",
+  route: "v1/webhooks/logs",
+})
+export class WebhookExecutionv1Route extends ServiceController {
+  public async [methods.POST](request: ApiRequest, response: ApiResponse) {
+    const headerAuth = request.headers?.authorization;
+
+    if (isNullish(headerAuth)) {
+      return response.unauthorized();
+    }
+
+    const webhook = await this.database.webhook.findUnique({
+      where: { value: headerAuth },
+      select: { type: true, id: true },
+    });
+
+    if (webhook == null) {
+      return response.notFound();
+    }
+
+    const { success, value } = schema.run(request.body);
+
+    if (!success) {
+      return response.badRequest("Bad request");
+    }
+
+    this.container.logger.info(`${request.headers["user-agent"]} made a request to a deprecated endpoint`);
+
+    // safety: value is nullchecked above
+    await sendLogPreview(this.client, webhook.type, webhook.id, value!.logsId.toString());
+
+    return response.noContent();
+  }
+}
+

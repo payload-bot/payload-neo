e30ce8b4cd5a9d68dbe6f205787846830b12721a
c43721 <rlahman.game@gmail.com>
2022-10-22 16:42:06 -0500

    feat: convert preconditions to v3

--- src/preconditions/OwnerOnly.ts
+++ src/preconditions/OwnerOnly.ts
@@ -1,11 +1,17 @@
-import { Precondition } from "@sapphire/framework";
+import { AllFlowsPrecondition } from "@sapphire/framework";
 import { envParseArray } from "@skyra/env-utilities";
-import type { Message } from "discord.js";
+import type { CommandInteraction, ContextMenuInteraction, Message } from "discord.js";
 
 const OWNERS = envParseArray("OWNERS");
 
-export class UserPrecondition extends Precondition {
-  public async run(message: Message) {
-    return OWNERS.includes(message.author.id) ? this.ok() : this.error({ context: { silent: true } });
+export class UserPrecondition extends AllFlowsPrecondition {
+  public override messageRun = (msg: Message) => this.#checkOwner(msg.author.id);
+
+  public override chatInputRun = (interaction: CommandInteraction) => this.#checkOwner(interaction.user.id);
+
+  public override contextMenuRun = (interaction: ContextMenuInteraction) => this.#checkOwner(interaction.user.id);
+
+  #checkOwner(authorId: string) {
+    return OWNERS.includes(authorId) ? this.ok() : this.error({ context: { silent: true } });
   }
 }

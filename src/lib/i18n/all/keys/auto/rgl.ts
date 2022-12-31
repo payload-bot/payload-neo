761372213fbf9ca932efd4eb46e8341cf1290a79
c43721 <rlahman.game@gmail.com>
2022-12-31 16:11:57 -0600

    feat(i18n): add translations for en-US to rgl preview

--- src/lib/i18n/all/keys/auto/rgl.ts
+++ src/lib/i18n/all/keys/auto/rgl.ts
@@ -1,6 +1,4 @@
 import { T } from "#lib/types";
 
 export const RGLDescription = T<string>("auto/rgl:description");
-export const RGLMatchesDescription = T<string>("auto/rgl:matchesDescription");
 export const RGLEmbedTitle = T<string>("auto/rgl:embedTitle");
-export const RGLMatchesEmbedTitle = T<string>("auto/rgl:matchesEmbedTitle");

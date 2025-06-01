f05797bd4fb8d84da7073223f71e7d7212192444
c43721 <rlahman.game@gmail.com>
2025-06-01 16:10:51 -0500

    feat: convert link to slash command

--- src/commands/Tf2/link.ts
+++ src/commands/Tf2/link.ts
@@ -1,55 +1,80 @@
-import type { CommandOptions } from "@sapphire/framework";
+import type { Command, CommandOptions } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
-import type { Message } from "discord.js";
-import { send } from "@sapphire/plugin-editable-commands";
 import { getSteamIdFromArgs } from "#utils/getSteamId.ts";
 import { PayloadCommand } from "#lib/structs/commands/PayloadCommand.ts";
 import { LanguageKeys } from "#lib/i18n/all";
 import { eq } from "drizzle-orm";
 import { user } from "#root/drizzle/schema.ts";
-
-const FLAGS = ["D", "d"];
+import { fetchT, getLocalizedData } from "@sapphire/plugin-i18next";
+import { MessageFlags } from "discord-api-types/v10";
+import { isNullishOrEmpty } from "@sapphire/utilities";
 
 @ApplyOptions<CommandOptions>({
   description: LanguageKeys.Commands.Link.Description,
   detailedDescription: LanguageKeys.Commands.Link.DetailedDescription,
-  flags: FLAGS,
 })
 export class UserCommand extends PayloadCommand {
-  override async messageRun(msg: Message, args: PayloadCommand.Args) {
-    const steamId = await args.pick("string").catch(() => null);
+  override async chatInputRun(
+    interaction: Command.ChatInputCommandInteraction,
+  ) {
+    const t = await fetchT(interaction);
+    const steamId = interaction.options.getString("steam-id")?.trim();
 
-    if (args.getFlags("D") || args.getFlags("d")) {
-      await this.database
-        .update(user)
-        .set({
-          steamId: null,
-        })
-        .where(eq(user.id, msg.author.id));
+    if (isNullishOrEmpty(steamId)) {
+      await interaction.reply({
+        flags: MessageFlags.Ephemeral,
+        content: t(LanguageKeys.Commands.Link.MissingId),
+      });
 
-      await send(msg, args.t(LanguageKeys.Commands.Link.Delete));
-      return;
-    }
-
-    if (!steamId) {
-      await send(msg, args.t(LanguageKeys.Commands.Link.MissingId));
       return;
     }
 
     const testResult = await getSteamIdFromArgs(steamId);
 
     if (testResult === null) {
-      await send(msg, args.t(LanguageKeys.Commands.Link.MalformedId));
+      await interaction.reply({
+        flags: MessageFlags.Ephemeral,
+        content: t(LanguageKeys.Commands.Link.MalformedId),
+      });
+
       return;
     }
 
     await this.database
       .update(user)
-      .set({
-        steamId: null,
-      })
-      .where(eq(user.id, msg.author.id));
+      .set({ steamId })
+      .where(eq(user.id, interaction.user.id));
 
-    await send(msg, args.t(LanguageKeys.Commands.Link.Success));
+    await interaction.reply({
+      flags: MessageFlags.Ephemeral,
+      content: t(LanguageKeys.Commands.Link.Success, { steamId: testResult }),
+    });
+
+    return;
+  }
+
+  public override registerApplicationCommands(registry: Command.Registry) {
+    const rootNameLocalizations = getLocalizedData(LanguageKeys.Commands.Link.Name);
+    const rootDescriptionLocalizations = getLocalizedData(this.description);
+
+    const steamIdNameLocalizations = getLocalizedData(LanguageKeys.Commands.Link.SteamIdName);
+    const steamIdDescriptionLocalizations = getLocalizedData(LanguageKeys.Commands.Link.SteamIdDescription);
+
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName(this.name)
+        .setDescription(rootDescriptionLocalizations.localizations["en-US"]!)
+        .setDescriptionLocalizations(rootDescriptionLocalizations.localizations)
+        .setNameLocalizations(rootNameLocalizations.localizations)
+        .addStringOption(
+          (input) =>
+            input
+              .setName("steam-id")
+              .setDescription(steamIdNameLocalizations.localizations["en-US"]!)
+              .setRequired(true)
+              .setNameLocalizations(steamIdNameLocalizations.localizations)
+              .setDescriptionLocalizations(steamIdDescriptionLocalizations.localizations),
+        )
+    );
   }
 }
--- src/lib/i18n/all/keys/commands/link.ts
+++ src/lib/i18n/all/keys/commands/link.ts
@@ -1,8 +1,11 @@
-import { T } from "#lib/types";
+import { FT, T } from "#lib/types";
 
+export const Name = T<string>("commands/link:name");
+export const SteamIdName = T<string>("commands/link:steamIdName");
 export const Description = T<string>("commands/link:description");
+export const SteamIdDescription = T<string>("commands/link:steamIdDescription");
 export const DetailedDescription = T<string>("commands/link:detailedDescription");
 export const MissingId = T<string>("commands/link:missingId");
 export const MalformedId = T<string>("commands/link:malformedId");
-export const Success = T<string>("commands/link:success");
+export const Success = FT<{ steamId: string }>("commands/link:success");
 export const Delete = T<string>("commands/link:delete");
